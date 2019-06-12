784c3e9ddaa7376404a073853b8d214841f12d52
Dani Palou <dani@moodle.com>
2019-06-12 10:36:48 +0200

    Initial commit

---
+++ lang/en/filter_applink.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'filter_applink', language 'en'
+ *
+ * @package    filter_applink
+ * @copyright  2019 Dani Palou <dani@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['filtername'] = 'Moodle App link';
+$string['pluginname'] = 'Moodle App link Filter';
+$string['privacy:metadata'] = 'The Moodle App link Filter plugin does not store any personal data.';
