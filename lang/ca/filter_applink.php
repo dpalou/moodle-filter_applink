eb9e5b53fb284fb1895524c8e1eb16f60d37373a
Dani Palou <dani@moodle.com>
2019-06-14 09:11:37 +0200

    Translate to spanish and catalan

---
+++ lang/ca/filter_applink.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'filter_applink', language 'ca'
+ *
+ * @package    filter_applink
+ * @copyright  2019 Dani Palou <dani@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['filtername'] = 'Enllaç a l\'app de Moodle';
+$string['pluginname'] = 'Filtre d\'Enllaç a l\'app de Moodle';
+$string['privacy:metadata'] = 'El connector filter_applink no emmagatzema informació personal.';
+$string['urlscheme'] = 'Esquema URL';
+$string['urlscheme_help'] = 'Si vols que s\'obri una app pròpia enlloc de l\'app oficial de Moodle, introdueix el seu esquema URL aquí.';
---
+++ lang/es/filter_applink.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'filter_applink', language 'es'
+ *
+ * @package    filter_applink
+ * @copyright  2019 Dani Palou <dani@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['filtername'] = 'Enlace a la app de Moodle';
+$string['pluginname'] = 'Filtro de Enlace a la app de Moodle';
+$string['privacy:metadata'] = 'La extensión filter_applink no almacena información personal.';
+$string['urlscheme'] = 'Esquema URL';
+$string['urlscheme_help'] = 'Si quieres abrir una app propia en vez de la app oficial de Moodle, introduce su esquema URL aquí.';
