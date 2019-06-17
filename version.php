e00c80bd50dd4c5a970ba1a3d99eda5c6ac90acd
Dani Palou <dani@moodle.com>
2019-06-17 10:40:02 +0200

    Bump version number

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019061400;          // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019061401;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300;          // Requires this Moodle version.
 $plugin->component = 'filter_applink';    // Full name of the plugin (used for diagnostics).
 $plugin->release   = '1.0.0';
